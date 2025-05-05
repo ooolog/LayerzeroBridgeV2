36d09880e6fbe9eb90e667c28eaa2361b2b7dbb5
liumengyue <liumengyue@vipkid.com.cn>
2025-05-05 18:16:28 +0800

    chore: 初始化项目规范

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  output: "export", // 添加静态导出配置
+  distDir: process.env.NODE_ENV === "development" ? ".next" : "dist", // 根据环境设置不同的输出目录
   images: {
-    domains: [],
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    unoptimized: true, // 禁用图片优化
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "encoding");
+    return config;
   },
 };
 
-module.exports = nextConfig;
-
+export default config;
